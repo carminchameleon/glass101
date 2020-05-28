10f33b0679b75eda6498fe48ed0d2c4bee74f0d2
carminchameleon <hwanggee1211@gmail.com>
2020-05-28 15:46:01 +0900

    minor fix

--- src/Redux/Actions/index.js
+++ src/Redux/Actions/index.js
@@ -1,20 +1,50 @@
+export const ADD_TO_CART = "ADD_TO_CART"
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART"
+export const MINUS_FROM_CART = "MINUS_FROM_CART"
+
+export const addToCart = ( items, product ) => (dispatch) => {
+  const cartItems = items.slice()
+    // 장바구니에 있는지 없는지를 체크
+  let productAlreadyIncart = false;
+  cartItems.forEach(item => {
+    if(item.id === product.id){
+    productAlreadyIncart = true;
+    item.count++
+  }        
+  });
+  if(!productAlreadyIncart){
+    cartItems.push({...product, count : 1})
+}
+  localStorage.setItem("cartItems",JSON.stringify(cartItems));
+  return dispatch({type: ADD_TO_CART,
+  payload : {
+    cartItems : cartItems 
+  }})
+}
+
+export const minusFromCart = ( items, product ) => (dispatch) => {
+  const cartItems = items.slice()
+    // 장바구니에 있는지 없는지를 체크
+  let productAlreadyIncart = false;
+  cartItems.forEach(item => {
+    if(item.id === product.id){
+    productAlreadyIncart = true;
+    item.count--
+  }        
+  });
+  localStorage.setItem("cartItems",JSON.stringify(cartItems));
+  return dispatch({type: MINUS_FROM_CART,
+  payload : {
+    cartItems : cartItems 
+  }})
+}
 
 
-export const addCartCounts = () => {
-  return {
-    type: "ADD_CART_COUNTS",
-  };
-};
-
-export const removeCartCounts = ()=> {
-  return {
-    type: "REMOVE_CART_COUNTS",
-  };
-};
-
-export const addProduct = (product : {}) => {
-  return {
-    type : "ADD_PRODUCT",
-    payload : product
-  }
+export const removeFromCart = ( items, product ) =>  (dispatch) =>{
+  const cartItems = items.slice().filter( item => item.id !== product.id);
+    localStorage.setItem('cartItems', cartItems )
+    return dispatch( { type : REMOVE_FROM_CART, payload :
+      {
+      cartItems
+    }})
 }
